b46ea90de5dd565fbf65d8dcdc42e91e00c51501
likearollinson <brad.w.rollinson@gmail.com>
2023-04-27 16:21:25 -0400

    update env in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,78 @@
-import {useState} from 'react';
-import { Container, Box } from '@chakra-ui/react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import TextInput from './components/TextInput';
-import KeywordsModal from './components/KeywordsModal';
+import { useState } from "react";
+import { Container, Box } from "@chakra-ui/react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import TextInput from "./components/TextInput";
+import KeywordsModal from "./components/KeywordsModal";
 
 const App = () => {
-  const[keywords,setKeywords] = useState('');
-  const [isOpen,setIsOpen] = useState(false);
-  const [loading,setLoading] = useState(false);
+    const [keywords, setKeywords] = useState("");
+    const [isOpen, setIsOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-  const extractKeywords = async (text) => {
-    setLoading(true);
-    setIsOpen(true);
+    const extractKeywords = async (text) => {
+        setLoading(true);
+        setIsOpen(true);
 
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
-      },
-      body: JSON.stringify({
-        model: 'text-davinci-003',
-        prompt: 'Extract keywords from this text. Make the first letter of each word uppercase and separate with commas.\n\n' + text + '',
-        temperature: 0.5,
-        max_tokens: 60,
-        frequency_penalty: 0.8,
-        presence_penalty: 0.0
-      }),
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${env.VITE_OPENAI_API_KEY}`,
+            },
+            body: JSON.stringify({
+                model: "text-davinci-003",
+                prompt:
+                    "Extract keywords from this text. Make the first letter of each word uppercase and separate with commas.\n\n" +
+                    text +
+                    "",
+                temperature: 0.5,
+                max_tokens: 60,
+                frequency_penalty: 0.8,
+                presence_penalty: 0.0,
+            }),
+        };
+
+        try {
+            const response = await fetch(env.VITE_OPENAI_API_URL, options);
+
+            const json = await response.json();
+
+            const data = json.choices[0].text.trim();
+
+            console.log(data);
+            setKeywords(data);
+            setLoading(false);
+        } catch (error) {
+            console.log(error);
+        }
     };
 
-    try{
-      const response = await fetch(import.meta.env.VITE_OPENAI_API_URL, options);
-  
-      const json = await response.json();
+    const closeModal = () => {
+        setIsOpen(false);
+    };
 
-      const data = json.choices[0].text.trim();
-
-      console.log(data)
-      setKeywords(data);
-      setLoading(false);
-    } catch(error){
-      console.log(error);
-    }
-  };
-
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-
-  return (
-    <Box bg='blue.500' color='white' height='100vh' width='100vw' paddingTop={130}>
-      <Container maxW='3xl' centerContent>
-        <Header />
-        <TextInput extractKeywords={extractKeywords}/>
-        <Footer />
-      </Container>
-      <KeywordsModal isOpen={isOpen} closeModal={closeModal} keywords={keywords} loading={loading}/>
-    </Box>
-  )
+    return (
+        <Box
+            bg='blue.500'
+            color='white'
+            height='100vh'
+            width='100vw'
+            paddingTop={130}
+        >
+            <Container maxW='3xl' centerContent>
+                <Header />
+                <TextInput extractKeywords={extractKeywords} />
+                <Footer />
+            </Container>
+            <KeywordsModal
+                isOpen={isOpen}
+                closeModal={closeModal}
+                keywords={keywords}
+                loading={loading}
+            />
+        </Box>
+    );
 };
 
 export default App;
